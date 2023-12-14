9f0b5c88f2b144fb9d3d81632790842d8d941cfa
caitlinboon <caitlinboon12@gmail.com>
2023-12-14 22:42:15 +0000

    unit test

--- Assets/Scripts/EditModeTests/EditorModeTestScript.cs
+++ Assets/Scripts/EditModeTests/EditorModeTestScript.cs
@@ -14,5 +14,30 @@ public class EditorModeTestScript
         UnityEditor.EditorApplication.isPlaying = false;
 
     }
+    [Test]
+    public void Test_lives()
+    {
+       int health(3);
+       Assert.AreEqual(expected: 3, actual: health);
 
+    }
+
+    [Test]
+    public void Test_lives_damage_1()
+    {
+       int health(3);
+        lives--;
+       Assert.AreEqual(expected: 2, actual: health);
+
+    }
+
+    [Test]
+    public void Test_lives_lowerthan0()
+    {
+       int health(0)
+       Assert.AreEqual(expected: 0, actual: health);
+
+    }
+
+    
 }
